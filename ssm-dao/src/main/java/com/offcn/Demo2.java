1e04a871cbd8913f85ffaf4cdb9d71f61846e8eb
javatest1 <luciano_xx@163.com>
2020-08-04 18:11:48 +0800

    第一次添加内容

---
+++ ssm-dao/src/main/java/com/offcn/Demo2.java
@@ -0,0 +1,12 @@
+package com.offcn;
+
+/**
+ * @author 刘晓轩
+ * @description ceshi
+ * @date 2020/8/4 18:07
+ */
+public class Demo2 {
+    public static void main(String[] args) {
+        System.out.println("你好，第一次添加");
+    }
+}
